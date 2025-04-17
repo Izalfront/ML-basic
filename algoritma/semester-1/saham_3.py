dfcfab49bd41d9696f952a3d6ac26bae792262ad
Muhammad Rizal Wahyudi <128357156+Izalfront@users.noreply.github.com>
2025-04-17 20:45:28 +0800

    123. Best Time to Buy and Sell Stock III

--- algoritma/semester-1/saham_3.py
+++ algoritma/semester-1/saham_3.py
@@ -1,8 +1,11 @@
-harga_saham = [1,2,3,4,5]
+harga_saham = [3,3,5,0,0,3,1,4]
 total_untung = 0
 
 for i in range(1, len(harga_saham)):
     if harga_saham[i] > harga_saham[i-1]:
         untung = harga_saham[i] - harga_saham[i-1]
         total_untung += untung
-print(total_untung)    
+        print(total_untung)    
+
+# batasi hanya 2x transaksi
+# berarti ada code < 3 untuk batas transaksi
